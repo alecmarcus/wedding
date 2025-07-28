8d172fffc961b836d51383151430e4a009b6fd88
Alec <18580335+alecmarcus@users.noreply.github.com>
2025-07-27 23:08:31 -0400

    image wip

--- src/app/components/Image.tsx
+++ src/app/components/Image.tsx
@@ -1,14 +1,23 @@
+// import { requestInfo } from "rwsdk/worker";
+
 type Props = {
   src: string;
   alt: string;
   width?: number;
   height?: number;
   loading?: "lazy" | "eager";
-  // sizes?: string;
   className?: string;
 };
 
 // const fileNameRegex = /\.[^/.]+$/;
+
+// const getSrcSet = (src: string, dpr = 2) => {
+//   const { headers } = requestInfo;
+//   const origin = headers.get("origin");
+
+//   const first = `${origin}/cdn-cgi/image/blur=100,dpr=0.1,quality=low,/${src}`;
+//   const final = `${origin}/cdn-cgi/image/blur=100,dpr=${dpr}/${src}`;
+// };
 
 export const Image = ({
   src,
@@ -16,35 +25,21 @@ export const Image = ({
   width,
   height,
   loading = "lazy",
-  // sizes = "100vw",
   className,
 }: Props) => {
-  // const srcWithoutExt = src.replace(fileNameRegex, "");
-
   return (
-    <picture className={className}>
-      {/* <source
-        type="image/avif"
-        srcSet={`${srcWithoutExt}.avif`}
-        sizes={sizes}
-      />
-      <source
-        type="image/webp"
-        srcSet={`${srcWithoutExt}.webp`}
-        sizes={sizes}
-      /> */}
-      <img
-        src={src}
-        alt={alt}
-        width={width}
-        height={height}
-        loading={loading}
-        decoding="async"
-        style={{
-          maxWidth: "100%",
-          height: "auto",
-        }}
-      />
-    </picture>
+    <img
+      className={className}
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      loading={loading}
+      decoding="async"
+      // style={{
+      //   maxWidth: "100%",
+      //   height: "auto",
+      // }}
+    />
   );
 };
